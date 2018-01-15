cac7bacb9927ab4ba571f8468bbd0ee213d6500b
Benjamin Kristensen <bnm12@users.noreply.github.com>
2018-01-15 19:23:20 +0100

    Add project files.

---
+++ VibeControl/MainForm.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace VibeControl
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SpeedControl = new System.Windows.Forms.TrackBar();
+            this.ToyName = new System.Windows.Forms.Label();
+            this.ScanBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.SpeedControl)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // SpeedControl
+            // 
+            this.SpeedControl.LargeChange = 10;
+            this.SpeedControl.Location = new System.Drawing.Point(25, 156);
+            this.SpeedControl.Maximum = 100;
+            this.SpeedControl.Name = "SpeedControl";
+            this.SpeedControl.Size = new System.Drawing.Size(213, 45);
+            this.SpeedControl.SmallChange = 5;
+            this.SpeedControl.TabIndex = 0;
+            this.SpeedControl.TickFrequency = 5;
+            this.SpeedControl.Scroll += new System.EventHandler(this.SpeedControl_Scroll);
+            // 
+            // ToyName
+            // 
+            this.ToyName.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ToyName.Location = new System.Drawing.Point(12, 9);
+            this.ToyName.Name = "ToyName";
+            this.ToyName.Size = new System.Drawing.Size(260, 144);
+            this.ToyName.TabIndex = 1;
+            this.ToyName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // ScanBtn
+            // 
+            this.ScanBtn.Location = new System.Drawing.Point(93, 207);
+            this.ScanBtn.Name = "ScanBtn";
+            this.ScanBtn.Size = new System.Drawing.Size(75, 23);
+            this.ScanBtn.TabIndex = 2;
+            this.ScanBtn.Text = "Scan";
+            this.ScanBtn.UseVisualStyleBackColor = true;
+            this.ScanBtn.Click += new System.EventHandler(this.ScanBtn_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.ScanBtn);
+            this.Controls.Add(this.ToyName);
+            this.Controls.Add(this.SpeedControl);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.ShowIcon = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Vibe Control";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.SpeedControl)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar SpeedControl;
+        private System.Windows.Forms.Label ToyName;
+        private System.Windows.Forms.Button ScanBtn;
+    }
+}
+
---
+++ VibeControl/MainForm.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Buttplug.Client;
+using Buttplug.Core;
+using Buttplug.Core.Messages;
+using static Buttplug.Client.DeviceEventArgs;
+
+namespace VibeControl
+{
+    public partial class MainForm : Form
+    {
+        private List<ButtplugClientDevice> Devices = new List<ButtplugClientDevice>();
+
+        private ButtplugWSClient _client;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void InitClient()
+        {
+            _client = new ButtplugWSClient("Test client");
+
+            _client.DeviceAdded += OnDeviceChanged;
+            _client.DeviceRemoved += OnDeviceChanged;
+            _client.ErrorReceived += OnError;
+
+            Connect();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InitClient();
+        }
+
+        private async void Connect()
+        {
+            try
+            {
+                if (_client != null)
+                {
+                    //await _client.Connect(new Uri("ws://localhost:12345/buttplug"), true);
+                    await _client.Connect(new Uri("ws://localhost:12345/buttplug"), true);
+                    await _client.RequestDeviceList();
+
+                    Devices = _client.getDevices().ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                OnError(this, new ErrorEventArgs(new Error(ex.Message, Error.ErrorClass.ERROR_UNKNOWN, 0)));
+            }
+        }
+
+        private void OnDeviceChanged(object sender, DeviceEventArgs e)
+        {
+            switch (e.Action)
+            {
+                case DeviceAction.ADDED:
+                    Devices.Add(e.Device);
+                    break;
+
+                case DeviceAction.REMOVED:
+                    Devices.RemoveAll(dev => dev.Index == e.Device.Index);
+                    break;
+            }
+            ToyName.Text = string.Join(Environment.NewLine, Devices.Select(d => d.Name));
+        }
+
+        private void OnError(object sender, ErrorEventArgs e)
+        {
+            MessageBox.Show(e.Message.ErrorMessage, "Error");
+            if (_client?.IsConnected ?? false)
+            {
+                Task.Run(async () => { await _client.Disconnect(); });
+            }
+            _client = null;
+        }
+
+        private void SpeedControl_Scroll(object sender, EventArgs e)
+        {
+            foreach (var dev in Devices)
+            {
+                if (dev.AllowedMessages.TryGetValue("VibrateCmd", out var attrs))
+                {
+                    try
+                    {
+                        uint vibratorCount = attrs.FeatureCount ?? 0;
+
+                        List<VibrateCmd.VibrateSubcommand> vibratorSettings = new List<VibrateCmd.VibrateSubcommand>();
+
+                        for (uint i = 0; i < vibratorCount; i++)
+                        {
+                            vibratorSettings.Add(new VibrateCmd.VibrateSubcommand(i, (double)SpeedControl.Value / 100));
+                        }
+                        _client.SendDeviceMessage(dev, new VibrateCmd(dev.Index, vibratorSettings));
+                    }
+                    catch
+                    {
+                    }
+                }
+            }
+        }
+
+        private async void ScanBtn_Click(object sender, EventArgs e)
+        {
+            if(_client == null)
+            {
+                InitClient();
+            }
+            bool success = await _client.StartScanning();
+        }
+    }
+}
